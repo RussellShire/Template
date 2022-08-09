0f48e1fe9c64c10e2c31937525b8b54afa556a91
RussellShire <russell.shire@gmail.com>
2022-08-09 14:41:24 +0100

    Adds an error and some untested work on the view

--- src/mvc/controller.js
+++ src/mvc/controller.js
@@ -13,4 +13,16 @@ export default class Controller {
         console.log(this.model.hello())
         console.log(this.view.hello())
     }
+
+    newTaskRequest() {
+        // Takes a description input from the view and tells the model to add a new task, tells the view to render the task list
+    }
+
+    taskMarkedAsCompleted() {
+        // Takes a description input from the view and tells the model to add a new task, tells the view to render the task list
+
+    }
+    resetTaskListRequest() {
+        //Takes a reset input from the view and tells the model to clear the task list. Tells the view to rerender the task list empty.
+    }
 }
--- src/mvc/view.js
+++ src/mvc/view.js
@@ -1,5 +1,56 @@
 export default class View {
+    constructor(){
+    this.submitButton = document.getElementById('submit');
+    this.taskList = document.getElementById('task-list');
+    this.submitButton.onclick = e => {
+        e.preventDefault()
+        console.log('test')}
+    }
+ 
+
     hello() {
         return "Hello, I am the view"
+    }
+
+    taskSubmitted(){
+        console.log('clicked') 
+        //Has an input box for new tasks, when tasks are added passes the description to the controller.
+
+
+    }
+
+
+    render(taskList){
+        /*Renders the description of each task on the tasklist alongside a button to mark the task as completed.
+        Completed tasks are rendered with a strikethrough.*/
+        // use create element li
+        taskList.forEach(task => {
+            newElement = document.createElement('li');
+            const description = taskList.description;
+            newElement.appendChild(document.createTextNode(description));
+
+            const classAttribute = document.createAttribute('class');
+            classAttribute.value = 'task'
+            newElement.setAttributeNode(classAttribute);
+            
+            document.taskList.appendChild(newElement);
+
+        }
+        )
+
+        const newElement = document.createElement('li'); 
+
+        // Render whole taskList dynamically, make each id uuid, add a button for mark completed set any marked as completed as stirkethrough
+        //maybe append element that adds a class that does strikethrough 
+    
+    }
+    
+    taskCompleted(){
+        // If the button that marks tasks as completed is triggered passes that task's uuid to the controller
+    // Event listener that passes the uuid to the controller when dynamically created, 
+}
+
+    taskListReset(){
+        // A reset button that when triggered tells the controller.
     }
 }
