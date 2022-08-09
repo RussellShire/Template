ea0893f3a7cb8fd0a7212c76be04d4d0a7af0423
RussellShire <russell.shire@gmail.com>
2022-08-09 22:23:43 +0100

    Renamed controller functions

--- src/mvc/view.js
+++ src/mvc/view.js
@@ -1,7 +1,7 @@
 export default class View {
     constructor(){
-    this.submitButton = document.getElementById('submit');
-    this.taskList = document.getElementById('task-list');
+    this.submitButton = document.getElementById('submit')
+    this.taskList = document.getElementById('task-list')
     this.submitButton.onclick = e => {
         e.preventDefault()
         console.log('test')}
@@ -20,25 +20,25 @@ export default class View {
     }
 
 
-    render(taskList){
+    render(taskList, controller){
         /*Renders the description of each task on the tasklist alongside a button to mark the task as completed.
         Completed tasks are rendered with a strikethrough.*/
         // use create element li
         taskList.forEach(task => {
-            newElement = document.createElement('li');
-            const description = taskList.description;
-            newElement.appendChild(document.createTextNode(description));
+            newElement = document.createElement('li')
+            const description = taskList.description
+            newElement.appendChild(document.createTextNode(description))
 
-            const classAttribute = document.createAttribute('class');
+            const classAttribute = document.createAttribute('class')
             classAttribute.value = 'task'
-            newElement.setAttributeNode(classAttribute);
+            newElement.setAttributeNode(classAttribute)
             
-            document.taskList.appendChild(newElement);
+            document.taskList.appendChild(newElement)
 
         }
         )
 
-        const newElement = document.createElement('li'); 
+        const newElement = document.createElement('li') 
 
         // Render whole taskList dynamically, make each id uuid, add a button for mark completed set any marked as completed as stirkethrough
         //maybe append element that adds a class that does strikethrough 
